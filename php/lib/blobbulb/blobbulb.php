2417bd8fa89c749801ac40ecba739941f1b31f51
shapoco <40152459+shapoco@users.noreply.github.com>
2025-01-29 00:51:59 +0900

    wip

--- php/lib/blobbulb/blobbulb.php
+++ php/lib/blobbulb/blobbulb.php
@@ -2,39 +2,63 @@
 
 namespace bllb;
 
+use Error;
+
 enum Op :int {
   case ARYSTA = 0x2c;
   case OBJSTA = 0x2d;
   case BLKEND = 0x3c;
   case DOCSTA = 0xbc;
   case DOCEND = 0xbd;
+  
+  case BOOL = 0x82;
+  case FLASE = 0x30;
+  case TRUE = 0x31;
+
   case U6D00 = 0x40;
   case U8 = 0x80;
   case U16 = 0x90;
   case U32 = 0xa0;
   case U64 = 0xb0;
+
   case I8 = 0x81;
   case I16 = 0x91;
   case I32 = 0xa1;
   case I64 = 0xb1;
+
   case STR4B = 0xa2;
   case STR1L = 0xc1;
   case STR2L = 0xd1;
   case STR4L = 0xe1;
   case STR8L = 0xf1;
+
+  case NULL = 0x20;
 }
 
-function obj2bllb($obj): string {
+function object2bllb($obj): string {
+  $ctx = new BlobBuilder();
+  variant($obj)->generateBlob($ctx);
+  return $ctx->flush();
+}
 
+function isLinearArray($obj): bool{
+  $i = 0;
+  foreach($obj as $key) {
+    if ($key !== $i++) return false;
+  }
+  return true;
 }
 
 function variant($value): IVariant {
-  if (is_array( $value )) {
-    if (isObject($value)) {
-      return new BObject($value);
+  if ($value instanceof IVariant) {
+    return $value;
+  }
+  else if (is_array( $value )) {
+    if (isLinearArray($value)) {
+      return new BArray($value);
     }
     else {
-      return new BArray($value);
+      return new BObject($value);
     }
   }
   else {
@@ -42,53 +66,48 @@ function variant($value): IVariant {
   }
 }
 
-class GenerationContext {
-  private string $buff;
-  
-  public function rawOp(Op $op): void {
-    $this->rawU8($op->value);
+class BlobBuilder {
+  private string $buff = '';
+
+  public function __construct() {
+    $this->rawOp(Op::DOCSTA);
+    $this->rawU32(0x624c6c42);
+    $this->rawU8(0x10);
+    $this->rawU8(0x00);
+    $this->rawU8(0x00);
+    $this->rawU8(0x00);
+  }
+
+  public function flush(): string {
+    $this->rawOp(Op::DOCEND);
+    $this->rawU32(0);
+    $this->rawU32(0);
+    return $this->buff;
   }
   
-  public function rawBlobOp(Op $baseOp, int $size): int {
-    if ($size <= 0xff) {
-      $this->rawU8($baseOp->value);
-      return 0;
+  public function pushPrimitive($value): void {
+    if (is_int($value) || is_float($value)) {
+      $this->pushNumber($value);
     }
-    else if ($size <= 0xffff) {
-      $this->rawU8($baseOp->value + 0x10);
-      return 1;
+    else if (is_string($value)) {
+      $this->pushStr($value);
     }
-    else if ($size <= 0xffffffff) {
-      $this->rawU8($baseOp->value + 0x20);
-      return 2;
+    else if (is_bool($value)) {
+      $this->pushBool($value);
+    }
+    else if (is_null($value)) {
+      $this->pushNull();
     }
     else {
-      $this->rawU8($baseOp->value + 0x30);
-      return 3;
+      throw new Error('Unsupported type.');
     }
   }
-  
-  public function rawU8(int $value): void {
-    $this->buff .= pack("C", $value);
-  }
-  
-  public function rawU16(int $value): void {
-    $this->buff .= pack("v", $value);
-  }
-  
-  public function rawU32(int $value): void {
-    $this->buff .= pack("V", $value);
-  }
 
-  public function rawU64(int $value): void {
-    $this->buff .= pack("V", $value & 0xffffffff); $value >>= 32;
-    $this->buff .= pack("V", $value & 0xffffffff);
-  }
-
-  public function intNum(int $value): void {
+  public function pushNumber(int $value): void {
     if (is_float($value) && floor($value) == $value) {
       $value = (int)floor($value);
     }
+    
     if (is_int($value)) {
       if (0 <= $value) {
         if ($value <= 0x3f) {
@@ -130,22 +149,75 @@ class GenerationContext {
         }
       }
     }
+    else {
+      // todo impl FP32, FP64.
+      throw new Error("Not implemented yet.");
+    }
   }
 
-  public function str(string $str): void {
-    $size = strlen($str);
+  public function pushStr(string $value): void {
+    $size = strlen($value);
     if ($size <= 4) {
       $this->rawOp(Op::STR4B);
-      $this->buff .= $str . str_repeat('\0', 4 - $size);
+      $this->buff .= $value . str_repeat('\0', 4 - $size);
     }
     else {
       $this->rawBlobOp(Op::STR1L, $size);
+      $this->buff .= $value;
+    }
+  }
+
+  public function pushBool(bool $val): void {
+    $this->rawOp($val ? Op::TRUE : Op::FLASE);
+  }
+
+  public function pushNull(): void {
+    $this->rawOp(Op::NULL);
+  }
+  
+  public function rawOp(Op $op): void {
+    $this->rawU8($op->value);
+  }
+  
+  public function rawU8(int $value): void {
+    $this->buff .= pack("C", $value);
+  }
+  
+  public function rawU16(int $value): void {
+    $this->buff .= pack("v", $value);
+  }
+  
+  public function rawU32(int $value): void {
+    $this->buff .= pack("V", $value);
+  }
+
+  public function rawU64(int $value): void {
+    $this->buff .= pack("V", $value & 0xffffffff); $value >>= 32;
+    $this->buff .= pack("V", $value & 0xffffffff);
+  }
+  
+  public function rawBlobOp(Op $baseOp, int $size): int {
+    if ($size <= 0xff) {
+      $this->rawU8($baseOp->value);
+      return 0;
+    }
+    else if ($size <= 0xffff) {
+      $this->rawU16($baseOp->value + 0x10);
+      return 1;
+    }
+    else if ($size <= 0xffffffff) {
+      $this->rawU32($baseOp->value + 0x20);
+      return 2;
+    }
+    else {
+      $this->rawU64($baseOp->value + 0x30);
+      return 3;
     }
   }
 }
 
 interface IVariant {
-  public function generateBllb(GenerationContext $ctx): void;
+  public function generateBlob(BlobBuilder $ctx): void;
 }
 
 class BArray implements IVariant {
@@ -156,10 +228,10 @@ class BArray implements IVariant {
     }
   }
 
-  public function generateBllb(GenerationContext $ctx): void {
+  public function generateBlob(BlobBuilder $ctx): void {
     $ctx->rawOp(Op::ARYSTA);
     foreach($this->buff as $v) {
-      $v->generateBllb($ctx);
+      $v->generateBlob($ctx);
     }
     $ctx->rawOp(Op::BLKEND);
   }
@@ -169,15 +241,15 @@ class BObject implements IVariant {
   private array $buff = array();
   public function __construct(array $obj) {
     foreach($obj as $k => $v) {
-      $buff[$k] = variant($v);
+      $this->buff[$k] = variant($v);
     }
   }
 
-  public function generateBllb(GenerationContext $ctx): void {
+  public function generateBlob(BlobBuilder $ctx): void {
     $ctx->rawOp(Op::OBJSTA);
     foreach($this->buff as $k => $v) {
-      variant($k)->generateBllb($ctx);
-      $v->generateBllb($ctx);
+      variant($k)->generateBlob($ctx);
+      $v->generateBlob($ctx);
     }
     $ctx->rawOp(Op::BLKEND);
   }
@@ -189,16 +261,9 @@ class BPrimitive implements IVariant {
     $this->value = $value;
   }
 
-  public function generateBllb(GenerationContext $ctx): void {
+  public function generateBlob(BlobBuilder $ctx): void {
+    $ctx->pushPrimitive($this->value);
   }
-}
-
-function isObject($obj): bool{
-  $i = 0;
-  foreach($obj as $key) {
-    if ($key !== $i++) return true;
-  }
-  return false;
 }
 
 ?>
--- php/www/json2bllb.php
+++ php/www/json2bllb.php
@@ -4,9 +4,7 @@ require_once __DIR__."/../lib/blobbulb/blobbulb.php";
 $json_text = file_get_contents('php://input');
 $json_obj = json_decode($json_text, true);
 
-$body = bllb\variant($json_obj);
-
 header('Content-Type: application/json');
-echo json_encode($json_obj);
+echo bllb\object2bllb($json_obj);
 
 ?>
